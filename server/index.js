d6d1077ec5ecf1db209bc0cf7e78da26d2304137
Mohab Mohamed Gamal <mohabgamal112233@gmail.com>
2023-05-03 02:19:26 +0300

    Farewell

--- server/index.js
+++ server/index.js
@@ -1,25 +1,28 @@
-import express  from "express";
-import bodyParser from 'body-parser'
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
 
-import postRoutes from "./routes/posts.js"
+dotenv.config();
 
 const app = express();
 
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
-app.use (bodyParser.json({ limit: "30mb", extended: true }));
-app.use (bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use (cors());
+app.use('/posts', postRoutes);
 
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
 
-app.use('/posts', postRoutes)
-
-const CONNECTION_URL = "mongodb+srv://master:mastermirror@cluster0.elhfa.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000
-
-mongoose.connect(CONNECTION_URL)
-    .then(() => { app.listen(PORT, () => console.log('listening on port ' + PORT)) })
-    .catch((error) => { console.log(error) });
-
-
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log('listening on port ' + PORT));
+  })
+  .catch(error => {
+    console.log(error);
+  });
